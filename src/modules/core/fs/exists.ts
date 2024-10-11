2ee760bbe1261596819a8a5d3e95db5a99057e1c
Pedro Veloso <86307207+pedrohaveloso@users.noreply.github.com>
2024-10-10 21:08:25 -0300

    remove

--- src/modules/core/fs/exists.ts
+++
@@ -1,6 +0,0 @@
-import { access } from "fs/promises";
-
-export const exists = async (filename: string): Promise<boolean> =>
-  await access(filename)
-    .then(() => true)
-    .catch(() => false);
--- src/modules/sniper/formatter.ts
+++
@@ -1,3 +0,0 @@
-export const priceNumber = (price: string): number => {
-  return Number(price.replace(/[^0-9]/g, ""));
-};
